a056c63cbe457b373c023aef56dcc6c3519261f1
Rodrigo Cândido <rodrigocan@gmail.com>
2023-12-15 18:05:07 -0300

    Page Metadata no Next.js (SEO)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,14 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import "./globals.css"
 
-const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" })
 
 export const metadata: Metadata = {
-  title: 'devstore',
+  title: {
+    template: "%s | devstore",
+    default: "devstore",
+  },
 }
 
 export default function RootLayout({
@@ -14,8 +17,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html className={inter.variable} lang='pt'>
-      <body className='bg-zinc-950 text-zinc-50 antialiased'>{children}</body>
+    <html className={inter.variable} lang="pt">
+      <body className="bg-zinc-950 text-zinc-50 antialiased">{children}</body>
     </html>
   )
 }
