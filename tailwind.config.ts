2102bc98b42cf2e81e833e9b5efbf6ea4e726946
Rodrigo Cândido <rodrigocan@gmail.com>
2023-12-15 14:51:17 +0000

    Loading na Home

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,19 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss"
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    fontFamily: {
-      sans: 'var(--font-inter)',
-    },
-    gridTemplateRows: {
-      app: 'min-content max-content',
+    extend: {
+      fontFamily: {
+        sans: "var(--font-inter)",
+      },
+      gridTemplateRows: {
+        app: "min-content max-content",
+      },
     },
   },
   plugins: [],
